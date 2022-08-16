ad848df8bdfbc2b6e61e3ab108dc290646d7b63f
Rachid-playa <trevokn@gmail.com>
2022-08-16 13:39:34 +0300

    just more routes

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+const users = [
+    {
+        name: 'Its Me',
+        email: 'hapakule@quiz-app.com',
+        password: '09876543'
+    },
+    {
+        name: 'Not Me',
+        email: 'neverhere@quiz-app.com',
+        password: '12345678'
+    }
+]
 
 //landing page
 app.get('/', (req, res) => {
@@ -17,7 +31,35 @@ app.get('/login', (req, res) => {
 
 // display signup page
 app.get('/signup', (req, res) => {
-    res.render('signup')
+    const user = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+    
+    res.render('signup', {error: false, user:user})
+})
+
+//process signup form
+app.post('/signup', (req, res) =>{
+    const user = {
+        name: req.body.fullname,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    }
+
+    if (user.password === user.confirmPassword) {
+        
+    } else {
+        
+        let message = 'Password/confirm password mismatch'
+
+        res.render('signup', {error: true, message: message, user:user})
+    }
+
+
 })
 
 
